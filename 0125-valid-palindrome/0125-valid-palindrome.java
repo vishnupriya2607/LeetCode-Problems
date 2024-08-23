dc13d99e13222de5b646e5da3ea8ee8484037582
Vishnupriya <125855591+vishnupriya2607@users.noreply.github.com>
2024-08-23 09:41:56 +0530

    Time: 4 ms (55.87%), Space: 44.6 MB (39.48%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        int flag=0;
+        s=s.toLowerCase();
+List<Character> list=new ArrayList<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char a=s.charAt(i);
+            if(a>='a'&&a<='z'||a>='0'&&a<='9')
+            list.add(a);
+        }
+        if(list.size()==0)
+        {
+            return true;
+        }
+       for(int i=0;i<list.size();i++)
+       {
+        if(list.get(i)==list.get(list.size()-i-1))
+        {
+            flag=1;
+        }
+        else
+        {
+            flag=0;
+            break;
+        }
+       }
+       if(flag==1)
+        return true;
+        else
+        return false;
+    }
+}
