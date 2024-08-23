b4e03753de286acef90dca49721d954cf374d7ad
Vishnupriya <125855591+vishnupriya2607@users.noreply.github.com>
2024-08-23 11:32:57 +0530

    Time: 2 ms (51.85%), Space: 41.3 MB (82.8%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isValid(String s) {
+        int c=0;
+        if(s.length()%2!=0)
+        return false;
+        Stack<Character> stack = new Stack<>();
+        for(char x:s.toCharArray())
+        {
+            if(x=='{'||x=='('||x=='[')
+            {
+                stack.push(x);
+                c++;
+            }
+            if(stack.size()>0)
+            {
+            if(x==')'&& stack.peek()=='(')
+            {
+                stack.pop();
+            }
+            if(x=='}'&& stack.peek()=='{')
+            {
+                stack.pop();
+            }
+            if(x==']'&& stack.peek()=='[')
+            {
+                stack.pop();
+            }
+            }
+            else
+            {
+                return false;
+            }
+        }
+if(stack.size()==0&&c==s.length()/2)
+return true;
+else
+return false;
+
+    }
+}
