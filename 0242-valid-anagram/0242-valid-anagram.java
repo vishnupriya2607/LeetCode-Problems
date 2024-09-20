aad984b340134fe01cac653eb50d9949acdcbd5c
Vishnupriya <125855591+vishnupriya2607@users.noreply.github.com>
2024-09-20 19:19:23 +0530

    Time: 1 ms (99.85%), Space: 43.4 MB (65.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int n=s.length();
-        int n1=t.length();
-        if(n==n1)
-        {
- char[] charArray = s.toCharArray();
-        Arrays.sort(charArray);
- char[] c= t.toCharArray();
-        Arrays.sort(c);
-        s=new String(charArray);
-        t=new String(c);
-        return s.equals(t);
-        }
-        else
-        {
+        int[] letterFrequency = new int[26];
+        if(s.length()!=t.length())
+        return false;
+        for (char c : s.toCharArray()) 
+            letterFrequency[c - 'a']++;
+        for (char c : t.toCharArray()) {
+            letterFrequency[c - 'a']--;
+        if(letterFrequency[c-'a']<0)
             return false;
         }
+        return true;
     }
 }
