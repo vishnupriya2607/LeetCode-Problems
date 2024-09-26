cbe58e9acbdcbf0120d845c7d9a7c552ac32bc74
Vishnupriya <125855591+vishnupriya2607@users.noreply.github.com>
2024-09-26 10:02:08 +0530

    Time: 3 ms (88.92%), Space: 42.5 MB (99.2%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,27 +1,45 @@
-import java.util.*;
-
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> map = new HashMap<>();
-        List<Integer> result = new ArrayList<>();
-        
-        // Count the frequency of each element in nums1
-        for (int num : nums1) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        
-        // Iterate through nums2 and check for matches
-        for (int num : nums2) {
-            if (map.getOrDefault(num, 0) > 0) {
-                result.add(num);
-                map.put(num, map.get(num) - 1); // Decrease count after using
+        int len1=nums1.length;
+        int len2=nums2.length;
+      
+            List<Integer> li=new ArrayList<>();
+        if(len1<=len2)
+        {
+            for(int i=0;i<nums1.length;i++)
+            {
+                for(int j=0;j<nums2.length;j++)
+                {
+                    if(nums1[i]==nums2[j])
+                    {
+                        nums2[j]=-1;
+                        li.add(nums1[i]);
+                        break;
+                    }
+                }
             }
         }
+        else
+        {
+            for(int i=0;i<nums2.length;i++)
+            {
+                for(int j=0;j<nums1.length;j++)
+                {
+                    if(nums2[i]==nums1[j])
+                    {
+                        nums1[j]=-1;
+                        li.add(nums2[i]);
+                        break;
+                    }
+                }
+            }
 
-        // Convert the result list to an array
-        int[] arr = new int[result.size()];
-        for (int i = 0; i < result.size(); i++) {
-            arr[i] = result.get(i);
+        }
+        int k=0;
+        int arr[]=new int[li.size()];
+        for(int i:li)
+        {
+            arr[k++]=i;
         }
         return arr;
     }
