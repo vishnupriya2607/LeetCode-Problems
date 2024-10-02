b5fd67e0d0e115e3650d534bc7096d0c6009eec0
Vishnupriya <125855591+vishnupriya2607@users.noreply.github.com>
2024-10-02 18:18:30 +0530

    Time: 32 ms (41.74%), Space: 58.3 MB (86.94%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
@@ -1,7 +1,13 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
         HashMap<Integer,Integer> map=new HashMap<>();
-       int arr1[]=Arrays.stream(arr).distinct().sorted().toArray();
+    Set<Integer> set=new HashSet<>();
+    for(int i=0;i<arr.length;i++)
+    {
+        set.add(arr[i]);
+    }
+     Integer[] arr1 = set.toArray(new Integer[0]);  // Convert to an array
+        Arrays.sort(arr1);  //
        for(int i=0;i<arr1.length;i++)
        {
         map.put(arr1[i],i+1);
