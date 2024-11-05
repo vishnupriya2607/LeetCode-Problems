0f22d20c0ca048f7b6383facb3fa78935023a7ff
Vishnupriya <125855591+vishnupriya2607@users.noreply.github.com>
2024-11-05 09:43:55 +0530

    Time: 3 ms (66.57%), Space: 53.5 MB (69.25%) - LeetHub

---
+++ 1039-find-the-town-judge/1039-find-the-town-judge.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int a1[]=new int[n+1];
+        int a2[]=new int[n+1];
+        for(int i=0;i<trust.length;i++)
+        {
+            int a=trust[i][0];
+            int b=trust[i][1];
+            a1[a]++;
+            a2[b]++;
+        }
+        int out=-1;
+       for(int i=0;i<n+1;i++)
+       {
+        if(a1[i]==0 && a2[i]==n-1)
+        {
+            out=i;
+        }
+       }
+    return  out;
+    }
+}
