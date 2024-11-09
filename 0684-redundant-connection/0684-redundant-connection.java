03840bc1809514184961a4a7a95852e47dc9377d
Vishnupriya <125855591+vishnupriya2607@users.noreply.github.com>
2024-11-09 18:55:19 +0530

    Time: 1 ms (75.65%), Space: 43.5 MB (29.72%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -0,0 +1,41 @@
+class Solution {
+    
+    public int[] findRedundantConnection(int[][] edges) {
+        int n=edges.length;
+        sol d=new sol(n+1);
+        for(int [] a:edges)
+        {
+            if(!d.union(a[0],a[1]))
+            {
+                return a;
+            }
+        }
+            return new int[0];
+        
+    }
+}
+class sol
+{
+     static int []parent;
+    public sol(int n)
+    {
+         parent=new int[n];
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+    
+    }
+    public int find(int k)
+    {
+        if(k==parent[k])return k;
+         return find(parent[k]);
+    }
+    public boolean union(int s,int d)
+    {
+        int x=find(s);
+        int y=find(d);
+        if(x==y)return false;
+        parent[x]=y;
+        return true;
+    }
+}
