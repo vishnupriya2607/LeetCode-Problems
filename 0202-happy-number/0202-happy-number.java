730917e17b46d328a42d12a09fe4c950dd683a76
Vishnupriya <vishnupriyar.22cse@kongu.edu>
2024-12-29 00:21:14 +0530

    Time: 1 ms (72.83%), Space: 40.8 MB (44.04%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isHappy(int n) {
+        // Use a set to detect cycles
+        Set<Integer> seen = new HashSet<>();
+
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getSumOfSquares(n);
+        }
+
+        return n == 1; // If we reach 1, the number is happy
+    }
+
+    private int getSumOfSquares(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+}
