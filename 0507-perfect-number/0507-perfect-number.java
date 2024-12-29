13c1db6089d84feec3baa612b25cbbda80154464
Vishnupriya <vishnupriyar.22cse@kongu.edu>
2024-12-29 09:26:25 +0530

    Time: 1 ms (96.13%), Space: 40.4 MB (63.35%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,12 +1,19 @@
 class Solution {
     public boolean checkPerfectNumber(int a) {
-        int sum=0;
-       for(int i=1;i<=a/2;i++){
+        if(a<=1)
+        return false;
+        int sum=1;
+        int n=(int) Math.sqrt(a);
+       for(int i=2;i<=n;i++){
 if(a%i==0)
 {
     sum=sum+i;
+
+if(i!=a/i)
+{
+    sum=sum+a/i;
 }
-       }
+   }   }
         
         
         if(sum==a)
