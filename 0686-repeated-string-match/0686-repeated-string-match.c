f59103f153c78d27aeeeaab23979de74650673ea
Vishnupriya <vishnupriyar.22cse@kongu.edu>
2025-01-01 15:31:36 +0530

    Time: 67 ms (23.08%), Space: 8.1 MB (92.31%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.c
+++ 0686-repeated-string-match/0686-repeated-string-match.c
@@ -1,34 +1,25 @@
-int repeatedStringMatch(char *a, char *b) {
-    int aLen = strlen(a);
-    int bLen = strlen(b);
-
-    // Calculate the minimum number of repetitions needed
-    int minReps = (bLen + aLen - 1) / aLen; // ceil(bLen / aLen)
-    int c = minReps;
-
-    // Create a buffer to temporarily hold the repeated string
-    char temp[2 * aLen + bLen + 1]; // Enough to hold `a` repeated twice plus `b`
-
-    // Repeat `a` `minReps` times and store in `temp`
-    temp[0] = '\0'; // Initialize to empty string
-    for (int i = 0; i < minReps; i++) {
-        strcat(temp, a);
+int repeatedStringMatch(char* a, char* b) {
+    int l1=strlen(a);
+    int l2=strlen(b);
+    int c=l1;
+    int k=1;
+    while(c<l2)
+    {
+c=c+l1;
+k++;
     }
-
-    // Check if `b` is a substring of `temp`
-    if (strstr(temp, b) != NULL) {
-        return c;
+    char temp[2*l1+l2+1];
+    temp[0]='\0';
+    for(int i=0;i<k;i++)
+    {
+        strcat(temp,a);
     }
-
-    // Add one more repetition of `a` to `temp`
-    strcat(temp, a);
-    c++;
-
-    // Check again if `b` is a substring
-    if (strstr(temp, b) != NULL) {
-        return c;
-    }
-
-    // If `b` is not a substring, return -1
+    if(strstr(temp,b)!=NULL)
+    return k;
+    strcat(temp,a);
+    k++;
+    if(strstr(temp,b)!=NULL)
+    return k;
     return -1;
+
 }
